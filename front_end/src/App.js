2f7c34c213bc8ff6c02a8bda8b736107f31e099d
trungnguyen <trungn21@student.ubc.ca>
2024-05-24 12:10:15 +0700

    add darkmode

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
 import Chat from './components/Chat/Chat';
 import FileUploader from './components/FileUpload/Upload';
 import SectionSwitchBar from './components/Section/Section';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div>
       <div className="row">
-        <div className="col-4"></div>
+        <div className="col-4 d-flex">
+          <button className='toggle-dark-mode' onClick={toggleDarkMode}>
+            {darkMode ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faMoon} />}
+          </button>
+        </div>
         <h1 className="text-center text-logo col-8"> Chat Document</h1>
       </div>
       
