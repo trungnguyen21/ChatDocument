05f21bc0da3bf7a222bdc632cb59737b44e10047
trungnguyen <trungn21@student.ubc.ca>
2024-06-12 20:08:41 +0700

    attempt to fix proxy

--- front_end/src/api.js
+++ front_end/src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://stingray-app-puj9w.ondigitalocean.app';
+axios.defaults.proxy.host = "https://stingray-app-puj9w.ondigitalocean.app"
 
 const api = axios.create({
-  baseURL: process.env.APP_API_URL,
+  baseURL: 'https://stingray-app-puj9w.ondigitalocean.app',
 });
 
 export default api;
