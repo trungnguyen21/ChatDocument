1ad7940257c6843ecb43529dc350efdafb38229a
xYelet <114847917+xYelet@users.noreply.github.com>
2024-12-02 00:11:52 -0800

    231

--- front_end/src/components/Chat/Chat.js
+++ front_end/src/components/Chat/Chat.js
@@ -12,17 +12,20 @@ const Chat = () => {
     { text: '4235223', sender: 'chatbot' },
     { text: 'wqewqewqrerqwrweqr', sender: 'user' },
     { text: 'wqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqw', sender: 'chatbot' },
-    { text: 'eeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqw.eeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqw', sender: 'user' },
+    { text: 'eeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqw.eeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqweeeeewqwqeqwewewqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqeqwewqeqw', sender: 'user' },
   ]);
   const [isActive, setActive] = useState(false);
   const messagesEndRef = useRef(null);
+  const inputRef = useRef(null);
   const { state } = useContext(ChatContext);
   const { notify } = useContext(ErrorContext);
   const session_id = state.sessionId;
   const baseURL = config.baseURL;
 
   const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
   };
 
   useEffect(() => {
@@ -37,12 +40,12 @@ const Chat = () => {
     const fetchChatHistory = async () => {
       if (state.sessionId) {
         try {
-          const response = await axios.get(baseURL + '/chat_history', { params: { session_id: state.sessionId } });
+          const response = await axios.get(baseURL + '/chat_history', {
+            params: { session_id: state.sessionId },
+          });
           const chatHistory = response.data.message;
           if (chatHistory.length === 0) {
-            setMessages([]);
-            setMessages((prevMessages) => [
-              ...prevMessages,
+            setMessages([
               {
                 text: 'Welcome! How can I help you today?',
                 sender: 'chatbot',
@@ -77,92 +80,86 @@ const Chat = () => {
     setMessages((prevMessages) => [...prevMessages, loadingMessage]);
 
     try {
-      console.log('Session ID at Chat.js:', session_id);
-
-      const response = await fetch(baseURL + `/chat_completion/?session_id=${session_id}&question=${text}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      const response = await fetch(
+        `${baseURL}/chat_completion/?session_id=${session_id}&question=${text}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
       const reader = response.body.getReader();
       const decoder = new TextDecoder();
       let chatbotMessage = { text: '', sender: 'chatbot' };
-      let loadingMessageIndex = messages.length + 1;
-      
+
       while (true) {
         const { value, done } = await reader.read();
         if (done) break;
         chatbotMessage.text += decoder.decode(value, { stream: true });
         setMessages((prevMessages) => {
           const updatedMessages = [...prevMessages];
-          updatedMessages[loadingMessageIndex] = { ...chatbotMessage };
+          updatedMessages[prevMessages.length - 1] = { ...chatbotMessage };
           return updatedMessages;
         });
       }
 
-      setMessages((prevMessages) => prevMessages.filter((_, index) => index !== loadingMessageIndex));
-
       setMessages((prevMessages) => [...prevMessages, chatbotMessage]);
-
     } catch (error) {
       console.error('Error getting response:', error);
-      const errorMessage = { text: 'Sorry, I encountered an error. Please try again. If the error is persistent, please reload the page.', sender: 'chatbot' };
+      const errorMessage = {
+        text: 'Sorry, I encountered an error. Please try again.',
+        sender: 'chatbot',
+      };
       setMessages((prevMessages) => {
         const updatedMessages = [...prevMessages];
-        const loadingMessageIndex = updatedMessages.findIndex(
-          (message) => message.sender === 'chatbot' && message.text === 'Generating a response...'
-        );
-        if (loadingMessageIndex !== -1) {
-          updatedMessages.splice(loadingMessageIndex, 1, errorMessage);
-        }
+        updatedMessages[prevMessages.length - 1] = errorMessage;
         return updatedMessages;
       });
     }
   };
 
-  const handleSendClick = (inputRef) => {
-    if (isActive && inputRef.current.value.trim() !== '') {
-      sendMessage(inputRef.current.value);
-      inputRef.current.value = '';
+  const handleInput = () => {
+    const input = inputRef.current;
+    if (input) {
+      input.style.height = 'auto'; // Reset height to calculate new height
+      input.style.height = `${Math.min(input.scrollHeight, 96)}px`; // Max height for 4 lines
     }
   };
 
-  const inputRef = useRef(null);
+  const handleSendClick = () => {
+    if (isActive && inputRef.current.value.trim() !== '') {
+      sendMessage(inputRef.current.value);
+      inputRef.current.value = '';
+      inputRef.current.style.height = 'auto'; // Reset height after sending
+    }
+  };
 
   return (
     <div className="chat-wrapper">
       <div className="messages-container d-flex flex-column">
-        
         {messages.map((message, index) => (
           <Message key={index} message={message} />
         ))}
         <div ref={messagesEndRef} />
-        
-        <div className="mt-auto">
-          <div className="input-container">
-            <textarea
-              ref={inputRef}
-              placeholder={isActive ? "Type a message..." : "Please upload or select a file."}
-              onKeyDown={(e) => {
-                if (isActive && e.key === 'Enter' && e.target.value.trim() !== '') {
-                  sendMessage(e.target.value);
-                  e.preventDefault()
-                  e.target.value = '';
-                }
-              }}
-              className="chat-input"
-            />
-            <button
-              onClick={() => handleSendClick(inputRef)}
-              className="btn-send"
-            >
-              <i className="bi bi-send"></i>
-            </button>
-          </div>
-        </div>
-        
+      </div>
+      <div className="input-container">
+        <textarea
+          ref={inputRef}
+          className="chat-input"
+          placeholder={isActive ? 'Type a message...' : 'Please upload or select a file.'}
+          onInput={handleInput} // Adjust height dynamically
+          onKeyDown={(e) => {
+            if (isActive && e.key === 'Enter' && e.target.value.trim() !== '') {
+              e.preventDefault(); // Prevent new line
+              handleSendClick();
+            }
+          }}
+        />
+        <button onClick={handleSendClick} className="btn-send">
+          <i className="bi bi-send"></i>
+        </button>
       </div>
     </div>
   );
