3c6dd8dd2ce61bd0ab837b150dc764669a1e01df
trungn21 <trungn21@student.ubc.ca>
2025-12-24 19:23:07 -0800

    fix docker

--- app/config/config.py
+++ app/config/config.py
@@ -3,18 +3,21 @@ from dotenv import load_dotenv
 from langchain.callbacks.streaming_stdout_final_only import FinalStreamingStdOutCallbackHandler
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
+APP_ENV = os.getenv("APP_ENV", "LOCAL")
 
-load_dotenv()
+if APP_ENV == "LOCAL":
+    print("Local dev mode detected")
+    load_dotenv("./env.local")
 
 class Config:
     def __init__(self):
         # Keys and Endpoints
-        self.GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-        self.REDIS_URL = os.getenv("REDIS_URL")
+        self.GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+        self.REDIS_URL = os.environ["REDIS_URL"]
         
         # Models
         self.LLM_MODEL = ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash", 
+            model=os.environ["LLM_MODEL"], 
             streaming=True, 
             callbacks=[FinalStreamingStdOutCallbackHandler(answer_prefix_tokens=["answer", ":"])]
         )
